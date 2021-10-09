057f38a8bd20a6f2f354844a803a5ed854f4e07a
Ashini Tharu <ashitharu99@gmail.com>
2021-10-09 11:58:10 +0530

    added  deleteCustomer method

---
+++ src/controller/CustomerController.java
@@ -0,0 +1,12 @@
+package controller;
+
+public class CustomerController {
+    public String saveCustomer(){
+        return ("customer saved");
+    }
+
+    public String deleteCustomer(){
+        return ("customer deleted");
+    }
+
+}
