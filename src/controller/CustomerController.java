0947b8a433e04c1943d4999b37bc078239c1be0d
Ashini Tharu <ashitharu99@gmail.com>
2021-10-09 11:37:50 +0530

    added updateItem method

---
+++ src/controller/CustomerController.java
@@ -0,0 +1,15 @@
+package controller;
+
+public class CustomerController {
+    public String saveCustomer(){
+        return ("customer saved");
+    }
+
+    public String deleteCustomer(){
+        return ("customer deleted");
+    }
+    public String updateCustomer(){
+        return ("customer updated");
+    }
+
+}
---
+++ src/controller/itemController.java
@@ -0,0 +1,15 @@
+package controller;
+
+public class itemController {
+    public String saveItem(){
+        return ("Item saved");
+    }
+
+    public String deleteItem(){
+        return ("Item deleted");
+    }
+    public String updateItem(){
+        return ("Item updated");
+    }
+
+}
